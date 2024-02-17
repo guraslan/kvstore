3341ed2cf143be94a796d27b43c80cdc9f01a27e
Yagmur Guraslan <yagmur@cloudertech.co.uk>
2024-02-17 15:57:29 +0000

    Add revision number for a key/value pair Api user should explicitly open the db store Create methods that work on a store type

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
+   s := kvstore.Open()
    if len(os.Args) < 2 {
-      fmt.Println(kvstore.Dump())
+      fmt.Println(s.Dump())
 	  os.Exit(0)
    }
    key := os.Args[1]
@@ -16,11 +17,11 @@ func main() {
    var value string
    if len(os.Args) == 3 {
 	 value = os.Args[2]
-	 kvstore.Store(key, value)
+	 s.Store(key, value)
 	 os.Exit(0)
    }
 
-   value = kvstore.Retrieve(key)
+   value = s.Retrieve(key)
    if value == "" {
       os.Exit(0)
    }
--- kvstore.go
+++ kvstore.go
@@ -6,31 +6,49 @@ import (
 )
 
 var storefile = "global.db"
-var store = map[string]string{}
 
-func init() {
-    readdb()
+type kv struct {
+	key      string
+	value    string
+	revision int
 }
 
-func readdb() error {
+type store []kv
+
+func Open() store {
+	s := make(store, 0)
+	// TODO: Unless it's permission denied accessing/creating
+	// is it safe to ignore the error?
+	s, _ = s.readdb()
+	return s
+}
+
+func (s store) readdb() (store, error) {
 	f, err := os.OpenFile(storefile, os.O_RDONLY|os.O_CREATE, 0700)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer f.Close()
 
 	for {
-		var k, v string
-		_, err := fmt.Fscanf(f, "%s\n%s\n", &k, &v)
+		var key, value string
+		var revision int
+		_, err := fmt.Fscanf(f, "%v\n%v\n%v\n", &key, &value, &revision)
 		if err != nil {
 			break
 		}
-		store[k] = v
+
+		pair := kv{
+			key:      key,
+			value:    value,
+			revision: revision,
+		}
+		s = append(s, pair)
 	}
-	return nil
+	return s, nil
 }
 
-func writedb() error {
+func (s store) writedb() error {
 	f, err := os.OpenFile(storefile, os.O_WRONLY|os.O_CREATE, 0700)
 	if err != nil {
 		return err
@@ -38,26 +56,50 @@ func writedb() error {
 	defer f.Close()
 
 	var buf string
-	for k, v := range store {
-		buf = buf + k + "\n" + v + "\n"
+	for _, pair := range s {
+		buf += fmt.Sprintf("%v\n%v\n%v\n", pair.key, pair.value, pair.revision)
 	}
 	f.Write([]byte(buf))
 	return nil
 }
 
-func Retrieve(key string) string {
-	return store[key]
+func (s store) Retrieve(key string) string {
+	i, err := s.findKV(key)
+	if err != nil {
+		return ""
+	}
+	return s[i].value
 }
 
-func Store(key, value string) {
-	store[key] = value
-	err := writedb()
+func (s store) findKV(key string) (int, error) {
+	for i, r := range s {
+		if r.key == key {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("not found")
+}
+
+func (s store) Store(key, value string) {
+	if i, err := s.findKV(key); err != nil {
+		s = append(s, kv{
+			key:   key,
+			value: value,
+		})
+	} else {
+		s[i] = kv{
+			key:      key,
+			value:    value,
+			revision: s[i].revision + 1,
+		}
+	}
+	err := s.writedb()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func Dump() map[string]string {
-	return store
+func (s store) Dump() store {
+	return s
 }
